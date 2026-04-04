6b04e46f30eab976d0073de8426607eafb70eb67
micheallakoan <micheallakoan@gmail.com>
2026-04-04 14:20:05 -0700

    setup backend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err))
+
+app.listen(3000, () => console.log('Server running on port 3000'))
