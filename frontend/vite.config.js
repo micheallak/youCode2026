06a234941b1f8e055bbeb1f23c2698e9818f7fb2
micheallakoan <micheallakoan@gmail.com>
2026-04-04 14:14:41 -0700

    add admin frontend, update frontend to be PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { VitePWA } from 'vite-plugin-pwa'
+ 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'youCode2026',
+        short_name: 'youCode',
+        description: 'youCode2026 app',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
