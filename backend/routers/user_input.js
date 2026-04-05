b921d2799799b20f71edb00f0efede44ad31559c
micheallakoan <micheallakoan@gmail.com>
2026-04-04 20:24:13 -0700

    add endpoint

--- backend/routers/user_input.js
+++ backend/routers/user_input.js
@@ -15,4 +15,14 @@ router.post('/', async (req, res) => {
     }
 })
 
+// GET
+router.get('/', async (req, res) => {
+    try {
+        const userInputs = await UserInput.find();
+        res.json(userInputs);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
 export default router;
