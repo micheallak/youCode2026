9edf1f451148491a02f0563e2c5e5b6ea363f00f
alicefu012 <alicefu012@gmail.com>
2026-04-04 17:07:37 -0700

    pushing backend connection code

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,7 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-export default connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI)
-    console.log('MongoDB connected')
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected');
+    } catch (error) {
+        console.error('❌ MongoDB connection failed:', error.message);
+        process.exit(1);
+    }
 }
+
+export default connectDB;
 
